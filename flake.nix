25827cab57a7ac8aaf6a695cb02779c19f1e0b26
chpxu <dev.chpxu@outlook.com>
2025-12-20 12:07:49 +0000

    dev: pin non-unstable deps

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   description = "Opinionated Flake for Fortran/Python/C/C++/JS Development";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    nixpkgs2505.url = "github:NixOS/nixpkgs/2b0d2b456e4e8452cf1c16d00118d145f31160f9"; # to use for older packages
+    flake-parts.url = "github:hercules-ci/flake-parts/a34fae9c08a15ad73f295041fec82323541400a9";
+    devshell.url = "github:numtide/devshell/17ed8d9744ebe70424659b0ef74ad6d41fc87071";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix/b68b780b69702a090c8bb1b973bab13756cc7a27";
   };
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs2505,
       flake-parts,
       devshell,
       git-hooks-nix,
