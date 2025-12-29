66f069a5bf57af91872bbfe56b6ce7cfbabeca6b
chpxu <dev.chpxu@outlook.com>
2025-12-29 21:42:24 +0000

    vscode/c: some reasonable default settings

--- nix/editors/vscode/languages/c.nix
+++ nix/editors/vscode/languages/c.nix
@@ -1,5 +1,31 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.languages.c;
+in
+{
   settings = {
-    
+    "C_Cpp.default.cppStandard" = "c++17";
+    "C_Cpp.default.compilerPath" = (
+      if cfg.llvm.enable then "${cfg.compiler}/bin/clang" else "${cfg.compiler}/bin/gcc"
+    );
+    "C_Cpp.default.cStandard" = "c23";
+    "C_Cpp.default.intelliSenseMode" = (if cfg.llvm.enable then "linux-clang-x64" else "linux-gcc-x64");
+    "C_Cpp.autocompleteAddParentheses" = true;
+  };
+  extensions = {
+    recommendations = [
+      "cschlosser.doxdocgen"
+      "ms-vscode.cpptools"
+      "franneck94.c-cpp-runner"
+    ]
+    ++ lib.optional cfg.cmake.enable [ "ms-vscode.cmake-tools" ]
+    ++ lib.optional cfg.meson.enable ["mesonbuild.mesonbuild"]
+    ++ lib.optional cfg.meson.enable ["mesonbuild.mesonbuild"]
+    ++ lib.optional cfg.gnumake.enable ["ms-vscode.makefile-tools"];
   };
 }
