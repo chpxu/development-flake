0a8fa44c2ce5c1a4324b25261b8455e019a5204d
chpxu <dev.chpxu@outlook.com>
2026-01-03 22:03:40 +0000

    python: nixPackages now accepts normal packages

--- nix/languages/python/default.nix
+++ nix/languages/python/default.nix
@@ -3,112 +3,107 @@
   config,
   inputs,
   ...
-}:
-let
+}: let
   t = lib.types;
-in
-{
-  perSystem =
-    { pkgs, config, ... }:
-    let
-      cfg = config.languages.python;
-    in
-    {
-      options.languages.python = {
-        enable = lib.mkOption {
-          type = t.bool;
-          default = false;
-          description = "Enable python in the environment.";
+in {
+  perSystem = {
+    pkgs,
+    config,
+    ...
+  }: let
+    cfg = config.languages.python;
+  in {
+    options.languages.python = {
+      enable = lib.mkOption {
+        type = t.bool;
+        default = false;
+        description = "Enable python in the environment.";
+      };
+      version = lib.mkOption {
+        type = t.enum [
+          "310"
+          "311"
+          "312"
+          "313"
+          "314"
+          "315"
+        ]; # Currently supported by NixOS 26.05
+        default = "312";
+        description = "The python version to use in the project, e.g \"310\" corresponds to Python 3.10.";
+      };
+      env = lib.mkOption {
+        type = t.listOf t.attrs;
+        default = [];
+        description = "Additional environment variables to add.";
+      };
+      nixPackages = lib.mkOption {
+        type = t.listOf t.package;
+        default = [];
+        description = "List of package attributes from python*Packages, e.g. with pythonPackages; [numpy scipy] etc.";
+      };
+      tools = lib.mkOption {
+        default = {
+          mypy = true;
         };
-        version = lib.mkOption {
-          type = t.enum [
-            "310"
-            "311"
-            "312"
-            "313"
-            "314"
-            "315"
-          ]; # Currently supported by NixOS 26.05
-          default = "312";
-          description = "The python version to use in the project, e.g \"310\" corresponds to Python 3.10.";
-        };
-        env = lib.mkOption {
-          type = t.listOf t.attrs;
-          default = [];
-          description = "Additional environment variables to add.";
-        };
-        nixPackages = lib.mkOption {
-          type = t.listOf t.package;
-          default = [ ];
-          description = "List of package attributes from python*Packages, e.g. with pythonPackages; [numpy scipy] etc.";
-        };
-        tools = lib.mkOption {
-          default = {
-            mypy = true;
-          };
-          type = t.submodule {
-            options = {
-              mypy = lib.mkOption {
-                type = t.bool;
-                default = true;
-                description = "Whether to add mypy to the environment.";
-              };
-              pylance = lib.mkOption {
-                type = t.bool;
-                default = false;
-                description = "Whether to add the Pylance language server to VSCode.";
-              };
+        type = t.submodule {
+          options = {
+            mypy = lib.mkOption {
+              type = t.bool;
+              default = true;
+              description = "Whether to add mypy to the environment.";
             };
-          };
-        };
-        uv = lib.mkOption {
-          default = {
-            enable = false;
-          };
-          type = t.submodule {
-            options = {
-              enable = lib.mkOption {
-                type = t.bool;
-                default = false;
-                description = "Enable managing python projects with uv instead of nixpkgs";
-              };
-              ruff = lib.mkOption {
-                description = "Install the ruff linter, also by the uv developers";
-                type = t.bool;
-                default = false;
-              };
+            pylance = lib.mkOption {
+              type = t.bool;
+              default = false;
+              description = "Whether to add the Pylance language server to VSCode.";
             };
           };
         };
       };
-      config = lib.mkIf cfg.enable {
-        devshells.python =
-          { extraModulesPath, ... }:
-          let
-            python = pkgs."python${cfg.version}";
-            pythonPackages = pkgs."python${cfg.version}Packages";
-            finalPythonPackages = (import ./packages.nix { inherit config pythonPackages lib; }).packages;
-            evaluateUV = import ./uv.nix {
-              inherit
-                pkgs
-                config
-                python
-                lib
-                ;
+      uv = lib.mkOption {
+        default = {
+          enable = false;
+        };
+        type = t.submodule {
+          options = {
+            enable = lib.mkOption {
+              type = t.bool;
+              default = false;
+              description = "Enable managing python projects with uv instead of nixpkgs";
             };
-          in
-          {
-            devshell = {
-              name = "python";
-              motd = "";
+            ruff = lib.mkOption {
+              description = "Install the ruff linter, also by the uv developers";
+              type = t.bool;
+              default = false;
             };
-            packages = lib.mkMerge [
-              (lib.mkIf cfg.uv.enable evaluateUV.packages)
-              [ (lib.mkIf (!cfg.uv.enable) (python.withPackages (_: finalPythonPackages))) ]
-            ];
-            env = cfg.env ++ (evaluateUV.env);
-
           };
+        };
       };
     };
+    config = lib.mkIf cfg.enable {
+      devshells.python = {extraModulesPath, ...}: let
+        python = pkgs."python${cfg.version}";
+        pythonPackages = pkgs."python${cfg.version}Packages";
+        finalPythonPackages = (import ./packages.nix {inherit config pythonPackages lib;}).genOptionals;
+        evaluateUV = import ./uv.nix {
+          inherit
+            pkgs
+            config
+            python
+            lib
+            ;
+        };
+      in {
+        devshell = {
+          name = "python";
+          motd = "";
+        };
+        packages =
+          lib.optionals cfg.uv.enable evaluateUV.packages
+          ++ lib.optionals (!cfg.uv.enable) [(python.withPackages (_: finalPythonPackages))]
+          ++ cfg.nixPackages;
+        env = cfg.env ++ (evaluateUV.env);
+      };
+    };
+  };
 }
--- nix/languages/python/packages.nix
+++ nix/languages/python/packages.nix
@@ -3,20 +3,16 @@
   config,
   lib,
   ...
-}:
-let
+}: let
   cfg = config.languages.python;
   # Get (alpha-sorted) list of tools
   # For each one, optionally return the package -> list of pythonPackages
   genOptionals = map (tool: lib.optionals cfg.tools.${tool} pythonPackages.${tool}) (
     builtins.attrNames cfg.tools
   );
-  concatOptionals = genOptionals; # flattened list of pythonPackages
-in
-{
-  packages = builtins.concatLists [
-    cfg.nixPackages
-    concatOptionals
-  ];
+in {inherit genOptionals;}
+# packages = builtins.concatLists [
+#   cfg.nixPackages
+#   concatOptionals
+# ];
 
-}
